b6393b434334f18c6ac6fc9290414687eb8e555b
Bruno <bruno_justino@icloud.com>
2016-06-23 21:06:59 -0300

    Iniciando uma loja

---
+++ produto-formulario.php
@@ -0,0 +1,13 @@
+<?php include("cabecalho.php") ;?>
+    <h1>Formulário de Cadastro</h1>
+<form action = "adiciona-produto.php">
+    Nome:
+    <input type= "text" name = "nome"><br/>
+    Preco:
+    <input type="number " name = "preco"><br/>
+
+    <input type = "submit" value = "Cadastrar"><br/>
+
+</form>
+
+<?php include("rodape.php") ;?>
